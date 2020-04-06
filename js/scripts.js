0372cbdd854979588e1adee90f5c1a86eccc3370
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-06 09:06:38 -0700

    create address book and contact objects, increment IDs

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// bs for address book 
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// bs for contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function(){
+  return this.firstName + " " + this.lastName;
+}
