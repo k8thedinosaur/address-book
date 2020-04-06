c1debb1c65ec7de251af7c49aa3a8d42c6bbc41b
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-06 09:22:19 -0700

    add method to delete contact, update functions to account for missing IDs

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,36 @@ function AddressBook() {
 }
 
 AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i])
+      if (this.contacts[i].id == id) { 
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
 }
 
 // bs for contacts
